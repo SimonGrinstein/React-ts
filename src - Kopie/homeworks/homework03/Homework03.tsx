6b6f7ae95b4126690179275a1fdd8942b77863d9
SimonGrinstein <senja@web.de>
2024-11-11 13:22:41 +0100

    del src copy

--- src - Kopie/homeworks/homework03/Homework03.tsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from "../../components/loginForm/LoginForm";
-
-function Homework03() {
-  return <LoginForm />;
-}
-
-export default Homework03;
--- src - Kopie/homeworks/homework04/Homework04.tsx
+++
@@ -1,6 +0,0 @@
-import Feedback from "../../components/feedback/Feedback";
-
-export default function Homework04() {
-    return <Feedback />;
-  }
-  

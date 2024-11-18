314178362fdb67f32c8fef7a4663fcca5fa9188c
SimonGrinstein <senja@web.de>
2024-11-18 11:43:57 +0100

    add Lesson12 and HW 12

--- src/lessons/lesson13/Lesson13.tsx
+++ src/lessons/lesson13/Lesson13.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import * as Yup from 'yup';
+import MyButton from '../../components/myButton/MyButton';
 
 export default function Lesson13() {
   return (
     <div className='lesson-container'>
         <h2>Lesson13</h2>
-        
+        <p>YUP validation library</p>
+        <form action=''>
+            <unput type = "text" />
+            <unput type = "text" />
+            <unput type = "text" />
+            <MyButton myType='submit' text='send' />
+
+        </form>
+
+
     </div>
   )
 }
